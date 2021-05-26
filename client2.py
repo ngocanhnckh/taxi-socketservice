70831ebb04e8cadcf79c6e33a4576c24946bdfda
Nguyen Ngoc Anh <anh.nguyen@youngit.org>
2021-05-27 03:08:49 +0700

    try to fix multithread

--- client2.py
+++ client2.py
@@ -2,7 +2,7 @@ import socket
 import time
 
 ClientSocket = socket.socket()
-host = '18.136.201.225'
+host = '127.0.0.1'
 port = 1234
 
 print('Waiting for connection')
@@ -16,6 +16,9 @@ lock = False;
 while True:
     time.sleep(1)
     Input = input('Say Something: ')
+    if (Input == 'stop'):
+        ClientSocket.close()
+        break
     ClientSocket.send(str.encode(Input))
     Response = ClientSocket.recv(1024)
     if(Response and (Response.decode('utf-8')!="ack" or Response.decode('utf-8')!="ackack")):
--- eventserver.py
+++ eventserver.py
@@ -15,11 +15,60 @@ try:
 except socket.error as e:
     print(str(e))
 
-print('Waitiing for a Connection..')
+print('Waiting for a Connection..')
 ServerSocket.listen(5)
 
 lock = threading.Lock()
 gMsg = ""
+
+
+class clientThread(threading.Thread):
+    def __init__(self, connection, killed=False):
+        threading.Thread(self)
+        
+
+    def run(self):
+        self.connection.send(str.encode('You are connected'))
+        global gMsg
+        msg = gMsg
+        try:
+            while True:
+                if(self.killed):
+                    return
+                lock.acquire()
+                
+                if (msg!=gMsg): 
+                    msg=gMsg
+                    self.connection.sendall(str.encode(msg))
+                    print(msg)
+                else:
+                    self.connection.sendall(str.encode("ack"))
+                
+                lock.release()
+                
+                data = self.connection.recv(2048)
+                reply = data.decode('utf-8')
+            
+                if (data and (reply!="heartbeat")):
+                    print(reply)
+                    lock.acquire()
+                    gMsg = reply
+                    lock.release()
+        except:
+            print("One connection died of error")
+            self.raise_exception()
+            try:
+                global ThreadCount
+                ThreadCount -= 1
+                self.connection.close()
+            except:
+                print("I hope Mr.Son won't see this error")
+
+
+    def raise_exception(self):
+        self.killed = True
+
+
 
 
 def threaded_client(connection):
@@ -50,14 +99,23 @@ def threaded_client(connection):
                 lock.release()
     except:
         print("One connection died of error")
-        connection.close()
-    connection.close()
+        try:
+            global ThreadCount
+            ThreadCount -= 1
+            connection.close()
+            return
+        except:
+            print("I hope Mr.Son won't see this error")
+        
+    
 
 while True:
     try:
         Client, address = ServerSocket.accept()
         print('Connected to: ' + address[0] + ':' + str(address[1]))
+        #t = clientThread((Client,))
         start_new_thread(threaded_client, (Client, ))
+        #t.start()
         ThreadCount += 1
         print('Thread Number: ' + str(ThreadCount))
     except:
